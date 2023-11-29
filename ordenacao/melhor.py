0252dd4160a4a9aab7955bf112586e7806a2749c
Vítor Assis <contatoassisvt@gmail.com>
2023-11-29 02:03:09 -0300

    Update melhor.py

--- ordenacao/melhor.py
+++ ordenacao/melhor.py
@@ -1,24 +1,20 @@
-Para conjuntos de dados pequenos:
+# Para conjuntos de dados pequenos:
+Algoritmos simples como Bubble Sort, Insertion Sort ou Selection Sort 
 
-Algoritmos simples como Bubble Sort, Insertion Sort ou Selection Sort podem ser eficientes devido à sua implementação direta.
-Para conjuntos de dados médios a grandes:
+# Para conjuntos de dados médios a grandes:
+Merge Sort e Quick Sort 
 
-Merge Sort e Quick Sort são geralmente eficientes. O Merge Sort é estável e funciona bem em conjuntos de dados encadeados, enquanto o Quick Sort é frequentemente mais rápido em prática devido ao seu particionamento eficiente.
-Para conjuntos de dados muito grandes ou dados externos:
+# Para conjuntos de dados muito grandes ou dados externos:
+Merge Sort
 
-Algoritmos externos como Merge Sort, que minimizam as leituras e gravações no disco, podem ser mais eficientes.
-Para conjuntos de dados com valores repetidos:
+# Para conjuntos de dados com valores repetidos:
+Merge Sort e Insertion Sort 
 
-Algoritmos estáveis como Merge Sort e Insertion Sort são preferíveis, pois mantêm a ordem relativa de elementos com chaves iguais.
-Para conjuntos de dados com um intervalo limitado de valores inteiros:
+# Para conjuntos de dados com um intervalo limitado de valores inteiros:
+Counting Sort ou Radix Sort 
 
-Counting Sort ou Radix Sort podem ser eficazes, já que eles exploram a estrutura dos números inteiros.
-Para conjuntos de dados parcialmente ordenados:
+#Se a estabilidade é crucial:
+Merge Sort, Insertion Sort, Timsort.
 
-Algoritmos adaptativos como TimSort (uma variação do Merge Sort usado no Python) podem ser eficazes, pois são otimizados para conjuntos de dados parcialmente ordenados.
-Se a estabilidade é crucial:
-
-Use algoritmos estáveis como Merge Sort, Insertion Sort, ou algoritmos baseados em árvores como o Timsort.
-Se a eficiência de espaço é uma prioridade:
-
-Radix Sort, Counting Sort, e algoritmos de ordenação baseados em troca como Bubble Sort podem ser preferíveis.
+# Se a eficiência de espaço é uma prioridade:
+Radix Sort, Counting Sort, Bubble Sort
